d2dba7d94191bf62a82389b43cc51111a81018df
thouficgit97 <f.thouficyousuf0987@gmail.com>
2024-03-16 13:44:54 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+var list=document.querySelector(".listItems")
+var text=document.getElementById("searchBar");
+function Add(){
+   
+    var task=document.createElement("p");
+    task.innerHTML+=text.value;
+    
+    // task.innerHTML+="<button onclick=\"Done()\" id=\"doneBtn\">✔</button> ";`
+
+     task.innerHTML+="&nbsp";
+    task.innerHTML+="<button id=\"dltBtn\" onclick=\"Delete(event)\">✖</button>";
+    if(text.value)
+        {
+          list.append(task);
+        }
+    else
+    {
+        alert("You have to Enter Something!")
+    }
+    text.value="";
+    
+}
+
+function Done()
+{
+    span.style.textDecoration="line-through";
+}
+function Delete(event){
+    
+    event.target.parentElement.remove();
+    // event.target.remove();
+}
